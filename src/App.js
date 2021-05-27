bcc38c85b71f9a545784c0031ae80365c9d2deed
Corey McCue <coreysmccue@gmail.com>
2021-05-27 12:38:08 +0200

    add commerce js logic to project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-// import { Products, Navbar } from './components';
-import Products from './components/Products/Products';
-import Navbar from './components/Navbar/Navbar';
+import { commerce } from './lib/commerce';
+
+import { Products, Navbar } from './components';
+
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    
+    setProducts(data);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products)
+
   return (
     <div>
       <Navbar />
