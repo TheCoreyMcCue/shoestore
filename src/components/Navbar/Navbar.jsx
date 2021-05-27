1091ed0c5ebc9fd09eeb31fa19df71c539383ed8
Corey McCue <coreysmccue@gmail.com>
2021-05-27 09:04:43 +0200

    create basic navbar design

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
-import { AppBar, ToolBar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
-import { ShoppingCard } from '@material-ui/icons';
-import classes from '*.module.css';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+import { ShoppingCard, ShoppingCart } from '@material-ui/icons';
+
+
+import logo from '../../assets/logo.png';
+import useStyles from './styles';
 
 const Navbar = () => {
+  const classes = useStyles();
+
   return (
     <>
       <AppBar position="fixed" className={classes.appBar} color="inherit">
-        <ToolBar>
-          <Typography>
-            <img src={} alt="Golden Shoe" />
+        <Toolbar>
+          <Typography variant="h6" className={classes.title} color="inherit">
+            <img src={logo} alt="Golden Shoe" height="25px" className={classes.image} />
+            Golden Shoe
           </Typography>
-        </ToolBar>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="show cart items" color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
       </AppBar>
     </>
   )
