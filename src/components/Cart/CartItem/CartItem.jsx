46ba19a15bae643745a0bb66b496bd9c597f0ed9
Corey McCue <coreysmccue@gmail.com>
2021-05-31 12:02:42 +0200

    imnplement basic stepper logic

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -3,7 +3,7 @@ import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@
 
 import useStyles from './styles';
 
-const CartItem = ({ item }) => {
+const CartItem = ({ item, handleRemoveFromCart, handleUpdateCartQty }) => {
   const classes = useStyles();
 
   return (
@@ -15,11 +15,11 @@ const CartItem = ({ item }) => {
       </CardContent>
       <CardActions className={classes.cartActions}>
         <div className={classes.buttons}>
-          <Button type="button" size="small">-</Button>
+          <Button type="button" size="small" onClick={() => handleUpdateCartQty(item.id, item.quantity - 1)}>-</Button>
           <Typography>{ item.quantity }</Typography>
-          <Button type="button" size="small">+</Button>
+          <Button type="button" size="small" onClick={() => handleUpdateCartQty(item.id, item.quantity + 1)}>+</Button>
         </div>
-        <Button variant="contained" type="button" color="secondary">Remove</Button>
+        <Button variant="contained" type="button" color="secondary" onClick={() => handleRemoveFromCart(item.id)} >Remove</Button>
       </CardActions>
     </Card>
   )
---
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const AddressForm = () => {
+  return (
+    <div>
+      Address Form
+      dfdsf
+       
+
+      <div>vvfdvfvfdvfvfd</div>
+    </div>
+   
+  )
+}
+
+export default AddressForm
