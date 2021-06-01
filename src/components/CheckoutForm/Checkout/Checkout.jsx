a83ded85fcb9b71305b60361f9316241a9e1101c
Corey McCue <coreysmccue@gmail.com>
2021-06-01 10:03:52 +0200

    add pay logic into payment form

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -41,7 +41,11 @@ const Checkout = ({cart}) => {
 
   const Form = () => activeStep === 0
     ? <AddressForm checkoutToken={checkoutToken} next={next} />
-    : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} />
+    : <PaymentForm
+        shippingData={shippingData}
+        checkoutToken={checkoutToken}
+        backStep={backStep}
+      />
 
   return (
     <>
